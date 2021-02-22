1a2af4ae3ada233aabdabda0dc239db8319629dd
Lukasz Chalinski <l.chalinski@gmail.com>
2021-02-22 10:33:56 +0100

    F025: first version of beats list - it will be waiting-room

--- beats/urls.py
+++ beats/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from beats.views import BeatDetailView, like_beat
+from beats.views import BeatDetailView, like_beat, AllBeatsList
 
 app_name = "beats"
 
 urlpatterns = [
     path("likes/", like_beat, name="like_beat"),
+
+    path('all/', AllBeatsList.as_view(), name='all_beats_list'), #todo: change this url to waiting-room for beats
     path("<int:pk>/<slug:slug>", BeatDetailView.as_view(), name="beat_detail"),
 ]
